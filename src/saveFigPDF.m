411f71aeed3d81bb038c454b4686790d084dbb1e
Jake Bowhay <jb9.bowhay@gmail.com>
2020-12-27 18:29:00 +0000

    explain section on outputting figures

--- src/saveFigPDF.m
+++ src/saveFigPDF.m
@@ -1,17 +1,18 @@
 function saveFigPDF(fileName)
-% saveFigPDF Save open figure as a PDF file
-%
-% Inputs:
-%   fileName = name to save the figure as eg. graph will
-%   save a file graph.pdf
-
-% get current figure handle
-figureHandle = gcf;
-% resize paper
-set(figureHandle,'PaperPosition',3*[0 0 6 4]);
-set(figureHandle,'PaperSize',3*[6 4]);
-set(figureHandle,'PaperUnits','centimeters');
-
-print(fileName,'-dpdf');
+    % saveFigPDF saves open figure as a PDF file
+    %
+    % Inputs:
+    %   fileName = File name to save figure as
+    % Usage:
+    %   saveFigPDF("polynomial") -> Saves current figure as polynomial.pdf
+    
+    % Get current figure handle
+    figureHandle = gcf;
+    % Resize paper
+    set(figureHandle,'PaperPosition',3*[0 0 6 4]);
+    set(figureHandle,'PaperSize',3*[6 4]);
+    set(figureHandle,'PaperUnits','centimeters');
+    
+    print(fileName,'-dpdf');
 end
 
