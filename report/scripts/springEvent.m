b4ba365824c0d722d462973421c4eb3ba3f08af0
Jake Bowhay <jb9.bowhay@gmail.com>
2020-12-27 17:58:36 +0000

    remove default files

--- report/scripts/springEvent.m
+++
@@ -1,8 +0,0 @@
-function [value,terminal,direction] = springEvent(t,y)
-
-value=y(2);     % look for y(2)=0
-direction=-1;   % with y(2) decreasing
-terminal=1;     % and stop the integration
-
-end
-
