29084ddd1fcfbd4a7fd451ad3fce56149b47877c
Jake Bowhay <jb9.bowhay@gmail.com>
2020-12-27 18:12:34 +0000

    improve comments

--- src/saveFigPDF.m
+++ src/saveFigPDF.m
@@ -1,13 +1,13 @@
 function saveFigPDF(fileName)
-%saveFigPDF Save open figure as a PDF file
+% saveFigPDF Save open figure as a PDF file
 %
-%Inputs:
+% Inputs:
 %   fileName = name to save the figure as eg. graph will
 %   save a file graph.pdf
 
-%get current figure handle
+% get current figure handle
 figureHandle = gcf;
-%resize paper
+% resize paper
 set(figureHandle,'PaperPosition',3*[0 0 6 4]);
 set(figureHandle,'PaperSize',3*[6 4]);
 set(figureHandle,'PaperUnits','centimeters');
